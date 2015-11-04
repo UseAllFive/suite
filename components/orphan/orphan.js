8303513d0481bf12c6fbb04d5a565de61b14a526
Bret Morris <morris.bret@gmail.com>
2015-11-04 14:21:34 -0800

    Allow orpahn to work for multiple paragraphs.

--- components/orphan/orphan.js
+++ components/orphan/orphan.js
@@ -4,8 +4,21 @@ angular.module('suite')
         return {
             restrict: 'A',
             link: function($scope, element, attrs) {
+                var $$element;
+                var $$p;
                 $timeout(function() {
-                    $(element).widowFix();
+                    $$element = $(element);
+                    $$p = $$element.find('p');                    
+                    if ($$p.length > 1) {
+                        // If there are multiple paragraphs contained within
+                        // element, then apply widowFix to each paragraph.
+                        $.each($$p, function(i, p) {
+                            $(p).widowFix();
+                        });
+                    } else {
+                        // Otherwise, apply widowFix to the entire element.
+                        $$element.widowFix();
+                    }                
                 });
             }
         };
