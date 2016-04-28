7d79759c84341784c098ecea89e2c8d818e6af82
Jason Farrell <jason@useallfive.com>
2016-04-28 16:51:43 -0700

    Add CanvasType module for multiline canvases.

---
+++ factories/canvas-type.js
@@ -0,0 +1,73 @@
+/* global angular */
+angular.module('suite')
+    .factory('CanvasTypeFactory', [function() {
+
+        function fillTextLine(context, text, x, y, letterSpacing) {
+
+            var characters = String.prototype.split.call(text, ''),
+            index = 0,
+            current,
+            currentPosition = x;
+
+            if (letterSpacing === 0) {
+                context.fillText(text, x, y);
+                return;
+            }
+
+            while (index < text.length) {
+                current = characters[index++];
+                context.fillText(current, currentPosition, y);
+                currentPosition += (context.measureText(current).width + letterSpacing);
+            }
+        }
+
+        function wrapText(context, text, x, y, maxWidth, lineHeight, maxLines, letterSpacing) {
+            var chars;
+            var currentNumLines;
+            if (typeof text === 'undefined') {
+                return 0;
+            }
+            chars = text.split('\n');
+            currentNumLines = 0;
+            for (var i = 0; i < chars.length; i++) {
+                var line;
+                var words;
+                line = '';
+                words = chars[i].split(' ');
+                for (var n = 0; n < words.length; n++) {
+                    var metrics;
+                    var testLine;
+                    var testWidth;
+                    testLine = line + words[n] + ' ';
+                    metrics = context.measureText(testLine);
+                    testWidth = metrics.width + (testLine.length * letterSpacing);
+                    if (testWidth > maxWidth) {
+                        currentNumLines++;
+                        if (currentNumLines > maxLines - 1) {
+                            line = line.substring(0, line.length - 3).slice(0, -2).trim();
+                            line += '…';
+                            fillTextLine(context, line, x, y, letterSpacing);
+                            //context.fillText(line, x, y);
+                            return y;
+                        }
+                        fillTextLine(context, line, x, y, letterSpacing);
+                        //context.fillText(line, x, y);
+                        line = words[n] + ' ';
+                        y += lineHeight;
+                    } else {
+                        line = testLine;
+                    }
+                }
+                fillTextLine(context, line, x, y, letterSpacing);
+                //context.fillText(line, x, y);
+                y += lineHeight;
+            }
+            return y - lineHeight;
+        }
+
+        return {
+            fillTextLine: fillTextLine,
+            wrapText: wrapText
+        };
+    }])
+;
